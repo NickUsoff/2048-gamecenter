0c7899d3d0221fb54dadf0cb79d137cf250f158e
nusoff01 <nicholas.usoff@tufts.edu>
2014-04-09 11:02:48 -0400

    added mongodb

--- webcenter.js
+++ webcenter.js
@@ -2,6 +2,17 @@ var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
 
+
+
+// Mongo initialization
+var mongoUri = process.env.MONGOLAB_URI ||
+  process.env.MONGOHQ_URL ||
+  'mongodb://localhost/database';
+var mongo = require('mongodb');
+var db = mongo.Db.connect(mongoUri, function (error, databaseConnection) {
+	db = databaseConnection;
+});
+
 app.use(logfmt.requestLogger());
 
 
